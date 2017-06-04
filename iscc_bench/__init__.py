754519b8d0d5458c556db9e1529e013c8024d1f1
Titusz Pan <titusz.pan@gmail.com>
2017-06-04 17:13:10 +0200

    add key property to MetaData

--- iscc_bench/__init__.py
+++ iscc_bench/__init__.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import os
 from collections import namedtuple
+from hashlib import sha1
 
 PACKAGE_DIR = os.path.dirname(__file__)
 DATA_DIR = os.path.join(PACKAGE_DIR, 'data')
@@ -11,3 +12,14 @@ MetaDataBase = namedtuple('Meta', 'isbn title author')
 
 class MetaData(MetaDataBase):
     """Go and extend if you like ..."""
+
+    @property
+    def key(self):
+        """The primary key of the metadata"""
+        return sha1(''.join(list(self)).encode('utf-8')).hexdigest()
+
+
+if __name__ == '__main__':
+    m = MetaData('9783906847122', 'Besondere Umstände', 'Kasperski, Gabrieala')
+    print(m)
+    print(m.key)
