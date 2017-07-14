89d5999a2b85f6f0280b67197fbec1a372b58059
Titusz Pan <titusz.pan@gmail.com>
2017-07-14 10:17:03 +0200

    count unicode normalized chars vs bytes

--- iscc_bench/title_length.py
+++ iscc_bench/title_length.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 """Script to measure title length statisics"""
+import unicodedata
 from itertools import cycle
 import numpy as np
 from iscc_bench.readers import ALL_READERS
@@ -23,17 +24,21 @@ def reject_outliers(data, m=2.):
 
 if __name__ == '__main__':
 
-    SAMPLE_SIZE = 1000000
+    SAMPLE_SIZE = 100000
 
     title_sizes = []
+    title_sizes_bytes = []
     for n, title in enumerate(iter_titles()):
-        title_sizes.append(len(title))
+        norm_title = unicodedata.normalize('NFKC', title)
+        title_sizes.append(len(norm_title))
+        title_sizes_bytes.append(len(norm_title.encode('utf-8')))
         if n > SAMPLE_SIZE:
             break
 
     data = np.array(title_sizes, dtype=np.uint16)
     abs_max = max(data)
     print('Longest title in {} samples had {} chars.'.format(SAMPLE_SIZE, abs_max))
+    print('Longest title in {} samples had {} bytes.'.format(SAMPLE_SIZE, max(title_sizes_bytes)))
     print('The mean title length of all titles is {} chars '.format(data.mean()))
 
     cleaned = reject_outliers(data)
