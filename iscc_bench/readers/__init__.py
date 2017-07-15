f53d6817b87bd24b2db92c854f45d2e66628e00d
Titusz Pan <titusz.pan@gmail.com>
2017-07-15 18:00:12 +0200

    Add libgen metadata reader

--- iscc_bench/readers/__init__.py
+++ iscc_bench/readers/__init__.py
@@ -4,6 +4,7 @@ from iscc_bench.readers.bxbooks import bxbooks
 from iscc_bench.readers.dnbrdf import dnbrdf
 from iscc_bench.readers.harvard import harvard
 from iscc_bench.readers.openlibrary import openlibrary
+from iscc_bench.readers.libgen import libgen
 
 
-ALL_READERS = (bxbooks, dnbrdf, harvard, openlibrary)
+ALL_READERS = (bxbooks, dnbrdf, harvard, openlibrary, libgen)
---
+++ iscc_bench/readers/libgen.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+"""Read data from libgen catalog dump
+
+Records: 1686652
+Size: 968 MB (uncompressed)
+Info: http://libgen.io/
+Data: http://libgen.io/content/libgen_content.rar
+
+Instructions:
+    Download datafile, unrar and place at:
+    ./data/libgen_content.csv
+"""
+import os
+import csv
+import logging
+import isbnlib
+from iscc_bench import DATA_DIR, MetaData
+
+
+log = logging.getLogger(__name__)
+
+
+LIBGEN_DATA = os.path.join(DATA_DIR, 'libgen_content.csv')
+
+
+def libgen(path=LIBGEN_DATA):
+
+    with open(LIBGEN_DATA, 'r', encoding='utf-8') as infile:
+        reader = csv.reader(infile, delimiter=',')
+        for row in reader:
+
+            title, creators, isbns = row[1].strip(), row[5].strip(), row[16].strip()
+
+            if not all((title, creators, isbns)):
+                log.warning('Skip incomplete record {}'.format(row[0]))
+                continue
+
+            creators = ';'.join(creators.split(','))
+
+            for s in isbns.split(','):
+                if not isbnlib.notisbn(s.strip()):
+                    isbn = isbnlib.to_isbn13(s.strip())
+                    yield MetaData(isbn, title, creators)
+                    break
+
+
+if __name__ == '__main__':
+    for entry in libgen():
+        print(entry)
