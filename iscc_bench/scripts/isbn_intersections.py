3fb84f451938f4b996ae54c7c5f642294e5d2321
Titusz Pan <titusz.pan@gmail.com>
2017-07-16 14:32:18 +0200

    Helper to create csv sample data with metadata pairs

--- iscc_bench/scripts/isbn_intersections.py
+++ iscc_bench/scripts/isbn_intersections.py
@@ -41,7 +41,7 @@ def load_isbns(reader):
 
     fp = os.path.join(iscc_bench.DATA_DIR, name + '.isbns')
     with open(fp) as f:
-        return f.read().split()
+        return [int(line.strip()) for line in f]
 
 
 def print_isbn_stats():
@@ -67,9 +67,78 @@ def get_intersecting_isbns():
     return set.intersection(*isbn_sets)
 
 
+def build_metadata_pairs(samples=100000):
+    """
+    Build sample data in format:
+        isbn, title_a, authors_a, title_b, authors_b
+
+    Where:
+        isbn is in both datasets
+        title_a + authors_a != title_b + authors_b
+    """
+    from itertools import cycle
+    import gc
+    gc.disable()
+
+    combos = [c for c in combinations(ALL_READERS, 2)]
+    samples_per_combo = int(samples/len(combos))
+    print("Creating ~%s samples per data source pair" % samples_per_combo)
+
+    fp = os.path.join(iscc_bench.DATA_DIR, 'metapairs_%s.sample' % samples)
+    total_samples = 0
+
+    with open(fp, 'wb') as outf:
+        for combo in combos:
+            gc.collect()
+            combo_name = '%s-%s' % (combo[0].__name__, combo[1].__name__)
+            a_isbns = set(load_isbns(combo[0]))
+            b_isbns = set(load_isbns(combo[1]))
+            relevant_isbns = a_isbns.intersection(b_isbns)
+            data = {}
+            counter = 0
+            reader_combo = cycle((combo[0](), combo[1]()))
+            print('Collecting %s combo' % combo_name)
+            for reader in reader_combo:
+                try:
+                    entry = next(reader)
+                except StopIteration:
+                    print('!!!! StopIteration')
+                    break
+
+                isbn = int(entry.isbn)
+
+                if isbn in relevant_isbns:
+                    title = entry.title
+                    author = entry.author
+                    if isbn not in data:
+                        data[isbn] = {'title': title, 'author': author}
+                        continue
+                    if title != data[isbn]['title'] or author != data[isbn]['author']:
+                        row = data[isbn]
+                        out_data = '{}|{}|{}|{}|{}\n'.format(
+                            isbn,
+                            row['title'].replace('|', ''),
+                            row['author'].replace('|', ''),
+                            title.replace('|', ''),
+                            author.replace('|', ''),
+                        )
+                        print(out_data)
+                        outf.write(out_data.encode('utf-8'))
+                        total_samples += 1
+                        relevant_isbns.remove(isbn)
+                        del data[isbn]
+                        if counter == samples_per_combo:
+                            print('Finished samples for %s' % combo_name)
+                            break
+                        if not relevant_isbns:
+                            print('Out of relevant ISBNs at %s samples for %s', (counter, combo_name))
+                            break
+                        counter += 1
+    print('Collected %s total samples' % total_samples)
+
 if __name__ == '__main__':
-    # print(reader_combinations())
-    # dump_isbns()
+    dump_isbns()
     print_isbn_stats()
-    # print('Total all intersecting isbns: {}'.format(len(get_intersecting_isbns())))
+    print('Total all intersecting isbns: {}'.format(len(get_intersecting_isbns())))
     print_data_source_intersections()
+    # build_metadata_pairs()
