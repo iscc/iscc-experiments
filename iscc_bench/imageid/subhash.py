485bbe732dfb8539ffaf017f3a005896a7f3e503
Titusz Pan <titusz.pan@gmail.com>
2019-02-10 22:18:55 +0100

    create subhash module

---
+++ iscc_bench/imageid/subhash.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""Test strategy with hashing mutiple shift invariant aligned patches
+
+See: https://stackoverflow.com/a/20316789/51627
+"""
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
