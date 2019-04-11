afa76e2643ed75c6864d2281afd3e220b848e487
Titusz Pan <titusz.pan@gmail.com>
2019-04-11 20:20:50 +0200

    Add various unicode spec helper functions

--- iscc_bench/textid/unicode_blocks.py
+++ iscc_bench/textid/unicode_blocks.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 """Blocks of unicode ranges"""
+import unicodedata
 from pprint import pprint
 
 import requests
@@ -7,6 +8,7 @@ URL = "https://www.unicode.org/Public/UCD/latest/ucd/Blocks.txt"
 
 
 def load_blocks():
+    """Load and parse unicode blocks from unicode standard"""
     blocks = {}
     data = requests.get(URL).text
     for line in data.splitlines():
@@ -17,6 +19,33 @@ def load_blocks():
     return blocks
 
 
+def codepoints():
+    """A list of all Unicode codepoints"""
+    cps = []
+    for block in load_blocks():
+        for cp in range(block[0], block[1] + 1):
+            cps.append(cp)
+    return cps
+
+
+def whitespace_codes():
+    """All whitespace character codes"""
+    ws = []
+    for cp in codepoints():
+        if unicodedata.category(chr(cp)) == 'Zs':
+            ws.append(cp)
+    return ws
+
+
+def control_codes():
+    """All control character codes"""
+    cc = []
+    for cp in codepoints():
+        if unicodedata.category(chr(cp)).startswith("C"):
+            cc.append(cp)
+    return cc
+
+
 if __name__ == '__main__':
-    b = load_blocks()
-    pprint(b)
+    pprint(load_blocks())
+
