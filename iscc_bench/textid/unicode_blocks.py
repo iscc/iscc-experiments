0d07f43f21595859f4abbd79ecee4f2f417a5a7e
Titusz Pan <titusz.pan@gmail.com>
2019-04-11 20:21:35 +0200

    Code formatting unicode_blocks module

--- iscc_bench/textid/unicode_blocks.py
+++ iscc_bench/textid/unicode_blocks.py
@@ -4,6 +4,7 @@ import unicodedata
 from pprint import pprint
 
 import requests
+
 URL = "https://www.unicode.org/Public/UCD/latest/ucd/Blocks.txt"
 
 
@@ -12,9 +13,9 @@ def load_blocks():
     blocks = {}
     data = requests.get(URL).text
     for line in data.splitlines():
-        if line and not line.startswith('#'):
-            hex_range, name = line.split(';')
-            int_range = tuple(int(i, 16) for i in hex_range.split('..'))
+        if line and not line.startswith("#"):
+            hex_range, name = line.split(";")
+            int_range = tuple(int(i, 16) for i in hex_range.split(".."))
             blocks[int_range] = name.strip()
     return blocks
 
@@ -32,7 +33,7 @@ def whitespace_codes():
     """All whitespace character codes"""
     ws = []
     for cp in codepoints():
-        if unicodedata.category(chr(cp)) == 'Zs':
+        if unicodedata.category(chr(cp)) == "Zs":
             ws.append(cp)
     return ws
 
@@ -46,6 +47,5 @@ def control_codes():
     return cc
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     pprint(load_blocks())
-
